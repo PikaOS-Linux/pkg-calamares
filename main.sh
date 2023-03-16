ab9b710d341070898eb68b581daa6b9d5c1034fd
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-16 17:57:35 +0300

    first commit

---
+++ main.sh
@@ -0,0 +1,26 @@
+DEBIAN_FRONTEND=noninteractive
+
+# Add dependent repositories
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+
+# Clone Upstream
+git clone https://github.com/calamares/calamares -b v3.3.0-alpha2
+cp -rvf ./debian ./calamares/
+cd ./calamares
+
+# Get build deps
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+apt-get build-dep ./ -y
+
+# Build package
+LOGNAME=root dh_make --createorig -y -l -p calamares_3.3.0-alpha2
+dpkg-buildpackage
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
