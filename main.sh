6214b757715e55faefbe632426e576a087e40e11
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:09:19 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,12 +2,6 @@
 set -e
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-
 # Clone Upstream
 #git clone https://github.com/calamares/calamares -b v3.3.0-alpha2
 #cp -rvf ./calamares2 ./calamares
@@ -15,8 +9,6 @@ cd ./calamares
 for i in ../patches/*; do patch -Np1 -i $i ;done
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata || true
 apt-get build-dep ./ -y || true
 
 # Build package
